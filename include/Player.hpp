826901f3299408999334b893d6140328aa364edb
naufaladrna08 <naufaladrn90@gmail.com>
2022-01-23 13:01:41 +0700

    added player class declaration

---
+++ include/Player.hpp
@@ -0,0 +1,28 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <Base.hpp>
+
+class Player {
+  public:
+    Player();
+    ~Player();
+
+    void Init(std::string textureName, sf::Vector2f position, float mess);
+    void Update(const float dt);
+    void Jump(float velocity);
+    sf::Sprite GetSprite();
+
+  private:
+    sf::Texture m_texture;
+    sf::Sprite m_sprite;
+    sf::Vector2f m_position;
+
+    int jumpCount = 0;
+    float m_mess;
+    float m_velocity;
+    const float m_gravity = 9.80f;
+    bool m_grounded;
+};
+
+#endif // __PLAYER_H__
