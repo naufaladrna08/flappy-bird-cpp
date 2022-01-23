28280045b55650b60f223a0cf15b8d3c75ac255e
naufaladrna08 <naufaladrn90@gmail.com>
2022-01-23 12:57:57 +0700

    standard input output and sfml

---
+++ include/Base.hpp
@@ -0,0 +1,12 @@
+#ifndef __BASE_H__
+#define __BASE_H__
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stack>
+
+/* SFML */
+#include <SFML/Graphics.hpp>
+
+#endif // __BASE_H__
